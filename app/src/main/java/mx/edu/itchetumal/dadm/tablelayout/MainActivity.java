ec729f14e60e7fdc66f70805b65e4c67f3575a60
yisus23 <choyn4@gmail.com>
2018-11-14 23:07:13 -0500

    cuarta version de una calculadora

--- app/src/main/java/mx/edu/itchetumal/dadm/tablelayout/MainActivity.java
+++ app/src/main/java/mx/edu/itchetumal/dadm/tablelayout/MainActivity.java
@@ -10,12 +10,16 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1, btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btn11,btn12;
-    EditText edtResult;
+    Button btnPorcentaje,btn1, btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btn11,btn12,btnTan,btnSen,btnCos,btnRaiz,btnLog,btnLn,btnPotencia,btnTanh,btnSenh,btnCosh;
+    EditText edtResult,edtConcatenar;
     Button btnBorrar;
-    String tempRes ;
+
     char operador1;
     int resultado;
+    double numero1;
+    double numero2;
+     double res;
+   int operador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -186,52 +190,149 @@ public class MainActivity extends AppCompatActivity {
                                     @Override
                                     public void onClick(View v) {
                                             try{
-                                                    tempRes = edtResult.getText().toString();
-                                                    edtResult.setText("");
-                                                    operador1 ='+';
-                                            }catch (NumberFormatException nfe){}
+                                                String tempRes  = edtResult.getText().toString();
+                                                numero1 = Double.parseDouble(tempRes);
 
+                                            }catch (NumberFormatException nfe){}
+                                        edtResult.setText("");
+                                        operador1 = 1;
 
                                     }
                                 }
 
 
         );
+
+
+
+        edtResult =(EditText) findViewById(R.id.edtResultado);
+        btnTan = (Button) findViewById(R.id.btnTan);
+        btnTan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    String tempRes  = edtResult.getText().toString();
+                    numero1 = Double.parseDouble(tempRes);
+                }catch (NumberFormatException nfe){}
+                edtResult.setText("tan (" + numero1 +")");
+                operador1 = 2;
+            }
+        });
+        edtResult =(EditText) findViewById(R.id.edtResultado);
+        btnSen = (Button) findViewById(R.id.btnSen);
+        btnSen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    String tempRes  = edtResult.getText().toString();
+                    numero1 = Double.parseDouble(tempRes);
+                }catch (NumberFormatException nfe){}
+                edtResult.setText("sen (" + numero1 +")");
+                operador1 = 3;
+            }
+        });
+        edtResult =(EditText) findViewById(R.id.edtResultado);
+        btnCos = (Button) findViewById(R.id.btnCos);
+        btnCos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    String tempRes  = edtResult.getText().toString();
+                    numero1 = Double.parseDouble(tempRes);
+                }catch (NumberFormatException nfe){}
+                edtResult.setText("cos (" + numero1 +")");
+                operador1 = 4;
+            }
+        });
+        edtResult =(EditText) findViewById(R.id.edtResultado);
+        btnPotencia = (Button) findViewById(R.id.btnPotencia);
+        btnPotencia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    String tempRes  = edtResult.getText().toString();
+                    numero1 = Double.parseDouble(tempRes);
+                }catch (NumberFormatException nfe){}
+                edtResult.setText("");
+                operador1 = 5;
+            }
+        });
+        edtResult =(EditText) findViewById(R.id.edtResultado);
+        btnPorcentaje = (Button) findViewById(R.id.btnPorcentaje);
+        btnPorcentaje.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    String tempRes  = edtResult.getText().toString();
+                    numero1 = Double.parseDouble(tempRes);
+                }catch (NumberFormatException nfe){}
+                edtResult.setText("");
+                operador1 = 5;
+            }
+        });
+        edtResult =(EditText) findViewById(R.id.edtResultado);
+        btnTanh = (Button) findViewById(R.id.btnTanh);
+        btnTanh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    String tempRes  = edtResult.getText().toString();
+                    numero1 = Double.parseDouble(tempRes);
+                }catch (NumberFormatException nfe){}
+                edtResult.setText("tanh ("+numero1+ ")");
+                operador1 = 6;
+            }
+        });
+        edtResult =(EditText) findViewById(R.id.edtResultado);
+        btnSenh = (Button) findViewById(R.id.btnSenh);
+        btnSenh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    String tempRes  = edtResult.getText().toString();
+                    numero1 = Double.parseDouble(tempRes);
+                }catch (NumberFormatException nfe){}
+                edtResult.setText("senh ("+numero1+")");
+                operador1 = 7;
+            }
+        });
+
+        edtResult =(EditText) findViewById(R.id.edtResultado);
+        btnCosh = (Button) findViewById(R.id.btnCosh);
+        btnCosh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    String tempRes  = edtResult.getText().toString();
+                    numero1 = Double.parseDouble(tempRes);
+                }catch (NumberFormatException nfe){}
+                edtResult.setText("cosh ("+numero1+")");
+                operador1 = 8;
+            }
+        });
+
+
+
+
         edtResult =(EditText) findViewById(R.id.edtResultado);
         btn12 = (Button) findViewById(R.id.btn12);
         btn12.setOnClickListener(new View.OnClickListener() {
                                     @Override
                                     public void onClick(View v) {
-                                         if (operador1=='+') {
-                                         resultado += Integer.parseInt( tempRes);
-                                         edtResult.setText(resultado+edtResult.toString());
+                                        try{
+                                            String tempRes1  = edtResult.getText().toString();
+                                            numero2 = Double.parseDouble(tempRes1);
 
-
-                                         }
+                                        }catch (NumberFormatException nfe){}
+                                        edtResult.setText("");
+                                        if (operador1 ==1){
+                                            res = numero1+numero2;
+                                        }
                                     }
                                 }
 
 
         );
-
-
-/*
-*  char operador='';
-*  boolean bandResulBorrador = false;
-*  if((operador!='')&& !bandResultado)
-*  edtResult.setText("1");
-*  bandResulBorrador = true;
-*
-*
-*
-*
-* */
-
-
-
-
-
-
     }
 
 }
